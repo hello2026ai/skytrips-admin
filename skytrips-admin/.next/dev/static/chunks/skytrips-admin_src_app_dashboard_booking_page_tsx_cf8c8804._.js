d5eefe449aca45cc211ae6ade99c5ed9f4e8ed49
saraswati-ama <saraswati@aurahimalaya.org>
2026-01-25 13:51:19 +0545

    feat: issued by auto filled and delete functionality added

--- skytrips-admin/.next/dev/static/chunks/skytrips-admin_src_app_dashboard_booking_page_tsx_cf8c8804._.js
+++ skytrips-admin/.next/dev/static/chunks/skytrips-admin_src_app_dashboard_booking_page_tsx_cf8c8804._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/skytrips-admin_src_06ad841b._.js",
+  "static/chunks/skytrips-admin_src_94802ea1._.js",
   "static/chunks/skytrips-admin_libs_shared-utils_constants_airline_json_b1a9e9d2._.js",
   "static/chunks/skytrips-admin_libs_shared-utils_constants_country_json_28ab32e0._.js",
   "static/chunks/f6dda_c0ba794d._.js"
